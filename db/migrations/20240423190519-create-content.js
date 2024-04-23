9b52444e4344ede0a3e7634cfd0e6d4845c1ddfa
pustuleczek <pustuleczek@outlook.com>
2024-04-23 23:14:37 +0200

    add database

---
+++ db/migrations/20240423190519-create-content.js
@@ -0,0 +1,43 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Contents', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      image_path: {
+        type: Sequelize.STRING,
+      },
+      text: {
+        type: Sequelize.TEXT,
+      },
+      parent_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'Parents',
+          },
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Contents');
+  },
+};
---
+++ db/models/parent.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Parent extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsToMany(models.Child, {
+        through: models.ParentChildren,
+        foreignKey: 'parent_id',
+      });
+      this.hasMany(models.Content, {
+        foreignKey: 'parent_id',
+      });
+    }
+  }
+  Parent.init({
+    login: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.ENUM('parent', 'child'),
+  }, {
+    sequelize,
+    modelName: 'Parent',
+  });
+  return Parent;
+};
