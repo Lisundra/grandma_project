004adb56664da2529a017053b4f623c873591002
pustuleczek <pustuleczek@outlook.com>
2024-04-24 11:39:26 +0200

    actual data

---
+++ src/views/components/Card.jsx
@@ -0,0 +1,30 @@
+const React = require('react');
+
+function Card({ card, login }) {
+  return (
+    <div className="card-container">
+      <div className="image-container">
+<img src={card.image_path} alt={Uploaded image: ${card.text}} />
+      </div>
+      <div className="text-container">
+        <div className="text">
+          {card.text}
+        </div>
+        <div className="text-btn">
+          <button type="button">
+            ОЗВУЧИТЬ ТЕКСТ
+          </button>
+        </div>
+        <div className="upload-btn">
+          <form action="/upload" method="post" encType="multipart/form-data">
+            <label htmlFor="imageUpload">ЗАГРУЗИТЬ КАРТИНКУ</label>
+            <input id="imageUpload" name="image" type="file" accept="image/*" />
+            <button type="submit">Отправить</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Card;
